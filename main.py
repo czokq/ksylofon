5fc1d9f4696ddcd090fd9d0f6850d5caf05f7f98
Uczeń <krzysztof.padala@wp.pl>
2023-09-27 12:35:17 +0200

    Utworzenie projektu

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print(1)
+import random
+liczba = random.randint(1, 10)
+
+x = input("podaj liczbę: ")
+
+
+if liczba == int(x):
+    print("wygrałeś")
+
+else :
+    print("przegrałes")
+
+print("ukrytą liczbą była:")
+print(liczba)
