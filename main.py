a45d49e2558de649b739fabf33b5a62f001baa78
Uczeń <krzysztof.padala@wp.pl>
2023-10-11 12:19:08 +0200

    wwqww

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import sys
 ocena = int(input("podaj swoją ocenę z klasy 8: "))#
+
 wynik = int(input("podaj swój wynik z  testu: "))
 
-if wynik >= 90:
-    print("dostałeś się do grupy zawansowanej")
+if ocena > 6 or ocena < 0 :
+    print("błendną informacją jest ocena", ocena)
     sys.exit(0)
 
-if ocena >= 5:
-    print("dostałeś się do grupy zawansowanej")
+elif wynik < 0 or wynik > 100:
+    print("błendną informacją jest wynik", wynik)
     sys.exit(0)
+
+if ocena >= 5 or wynik > 90 :
+        print("dostałeś się do grupy zawansowanej")
+        sys.exit(0)
 else:
-    print("dostałeś się do grupy niezawannsowanej")
-    sys.exit(0)
+        print("dostałeś się do grupy niezawannsowanej")
+        sys.exit(0)
+
