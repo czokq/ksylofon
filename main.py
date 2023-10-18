b37664dc9533b2a68b6f95fe300b454bd1fcdf4b
Uczeń <krzysztof.padala@wp.pl>
2023-10-18 11:43:26 +0200

    wwqww

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-import sys
-ocena = int(input("podaj swoją ocenę z klasy 8: "))#
+import turtle
 
-wynik = int(input("podaj swój wynik z  testu: "))
-
-if ocena > 6 or ocena < 0 :
-    print("błendną informacją jest ocena", ocena)
-    sys.exit(0)
-
-elif wynik < 0 or wynik > 100:
-    print("błendną informacją jest wynik", wynik)
-    sys.exit(0)
-
-if ocena >= 5 or wynik > 90 :
-        print("dostałeś się do grupy zawansowanej")
-        sys.exit(0)
-else:
-        print("dostałeś się do grupy niezawannsowanej")
-        sys.exit(0)
+wn = turtle.Screen()
+wn.bgcolor("white")
 
--- w.py
+++ w.py
@@ -1,26 +1,25 @@
-import sys
 import random
 import time
 
-liczba_szans = 3
-liczba = random.randint(1, 10)
-print("grasz o 20psc i lipton brzoskwinia :)")
-time.sleep(2)
-while liczba_szans > 0:
+liczba_szans = 3        #ilość szans
+liczba = random.randint(1, 10)  #losuje liczbe od 1 do 10
+print("grasz o 20psc i lipton brzoskwinia :)") # wygrana
+time.sleep(2)    #kod idzie spać na dwie sekundy
+while liczba_szans > 0:  #sprawdza czy zostały ci sznase
 
-    print(f"Masz {liczba_szans} szanse :)")
-    x = int(input("Podaj liczbę naturalną (mniejszą niż 10): "))
+    print(f"Masz {liczba_szans} szanse :)") # podaje ilość szans
+    x = int(input("Podaj liczbę naturalną (mniejszą niż 10): ")) #pobiera liczbę o którą sprawdza
 
-    if x == 0 or x > 10:
+    if x == 0 or x > 10: # sprawdza czy użytkownik potrafi czytać
         print("przeczytaj polecenie jeszcze raz :)")
 
 
-    if liczba == x:
-        print("brawo!!!! Wygrałeś")
+    if liczba == x: #sprawdza czy liczba podana przez uztkownika jest taka sama jak wylosowana
+        print("brawo!!!! Wygrałeś 20 psc i pół liptona")
         break
-    else:
+    else: #jeśli nie jest:
         print("Przegrałeś")
 
-    liczba_szans -= 1
+    liczba_szans -= 1 #odejmuje jedna szanse
 
-print("Ukrytą liczbą była:", liczba)
+print("Ukrytą liczbą była:", liczba) #podaje ukrytą liczbę
