39de1c0a2ef88b18d703b67ddcb8cab5632c5bfe
Uczeń <krzysztof.padala@wp.pl>
2023-10-30 10:29:48 +0100

    wwqww

--- 12.py
+++ 12.py
@@ -13,11 +13,11 @@ if ujp < 0 or jo < 0 or ujp < 0 or d < 0:
 wynik = (jp + jo + ujp + d) / 4
 
 
-if jp > 30 and jo > 30 and ujp > 30 and ujo > 30 and d > 30:
+if jp >= 30 and jo >= 30 and ujp >= 30 and ujp >= 30 and d >= 30:
     print("zdałeś bez amnesti")
     sys.exit(0)
 
-if wynik > 30:
+elif wynik > 30:
     print("zdałeś z amnestią")
 
 else:
