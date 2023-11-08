4ac07abfaef67efb63193aa533ce2592de6b98c7
Uczeń <krzysztof.padala@wp.pl>
2023-11-08 12:13:27 +0100

    wwqww

--- zadanie for i in/zadanie 15.py
+++ zadanie for i in/zadanie 15.py
@@ -1,3 +1,10 @@
-for x in range(3, 103, 3):
+#a
+'''for x in range(3, 103, 3):
+    y = 1/2*x+3
+    print("{}\t{}".format(x, y))'''
+
+#b
+for i in range(-30, 61, 1):
+    x= i / 10
     y = 1/2*x+3
     print("{}\t{}".format(x, y))
