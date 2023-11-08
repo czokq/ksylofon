6a7d2fee6c04fb61139feaec193e615fc2e6d0c7
Uczeń <krzysztof.padala@wp.pl>
2023-11-08 12:26:24 +0100

    wwqww

--- zadanie for i in/zadanie 15.py
+++ zadanie for i in/zadanie 15.py
@@ -3,8 +3,20 @@
     y = 1/2*x+3
     print("{}\t{}".format(x, y))'''
 
-#b
+#b)
+#I)
+'''
 for i in range(-30, 61, 1):
     x= i / 10
     y = 1/2*x+3
+    print("{}\t{}".format(x, y))'''
+#II)
+x = -3
+lista = [1]
+for i in lista:
+    y = 1/2 * x + 3
     print("{}\t{}".format(x, y))
+    x = x + 0.1
+    if x > 6:
+        break
+    lista.append(1)
