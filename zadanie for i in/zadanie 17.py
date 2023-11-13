2cd896c1f54d03824c2dce555b0b8d857644b6d3
Uczeń <krzysztof.padala@wp.pl>
2023-11-13 10:28:39 +0100

    wwqww

--- zadanie for i in/zadanie 17.py
+++ zadanie for i in/zadanie 17.py
@@ -1,7 +1,8 @@
 n = int(input("podaj ile ma byc liczb: "))
 suma = 0
-
+iloczyn =
 for i in range(n):
     liczba = float(input("podaj liczbe nr {} ".format(i + 1)))
     suma = suma + liczba
 print("suma = {}".format(suma))
+print("srednia = {}".format(suma / n))
